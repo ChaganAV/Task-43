64b8e5539afd597b7f3a571a252555427bf0adb0
Alexandr <jwchaganav@gmail.com>
2022-11-16 11:55:14 +0400

    Получение чисел от пользователя

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int k1 = ReadIntFromConsole($"Введите значение k1");
+int b1 = ReadIntFromConsole($"Введите значение b1");
+int k2 = ReadIntFromConsole($"Введите значение k2");
+int b2 = ReadIntFromConsole($"Введите значение b2");
+
+// Функции
+int ReadIntFromConsole(string message)
+{
+    Console.WriteLine(message);
+    int num = 0;
+    if(!Int32.TryParse(Console.ReadLine(), out num))
+        Console.WriteLine("Необходимо ввести число!");    
+    return num;
+}
