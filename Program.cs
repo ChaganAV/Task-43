12ccbfbe616e66cd4df64c99919752f1087b0bd5
Alexandr <jwchaganav@gmail.com>
2022-11-16 15:12:12 +0400

    Calculation

--- Program.cs
+++ Program.cs
@@ -3,12 +3,30 @@ int b1 = ReadIntFromConsole($"Введите значение b1");
 int k2 = ReadIntFromConsole($"Введите значение k2");
 int b2 = ReadIntFromConsole($"Введите значение b2");
 
+Console.WriteLine($"Уравнения: y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine($"Ваши значения: {k1},{b1},{k2},{b2}");
+Calculation();
+
 // Функции
+void Calculation()
+{
+    Console.WriteLine($"y = {k1}x + {b1}, y = {k2}x + {b2}");
+    Console.WriteLine($"y - y = {k1}x + {b1} - ({k2}x + {b2})");
+    Console.WriteLine($"{k1}x + {b1} = {k2}x + {b2}");
+    Console.WriteLine($"{k1-k2}x = {b1-b2}");
+    double x = (double)(b1-b2)/(k1-k2);
+    Console.WriteLine($"x = {x}");
+    double y = (double)k2*x + b2;
+    Console.WriteLine($"y = {k2}*{x} + {b2}");
+    Console.WriteLine($"y = {y}");
+    Console.WriteLine($"Точка пересечения: ({x}; {y})");
+}
+
 int ReadIntFromConsole(string message)
 {
     Console.WriteLine(message);
     int num = 0;
-    if(!Int32.TryParse(Console.ReadLine(), out num))
-        Console.WriteLine("Необходимо ввести число!");    
+    if (!Int32.TryParse(Console.ReadLine(), out num))
+        Console.WriteLine("Необходимо ввести число!");
     return num;
 }
